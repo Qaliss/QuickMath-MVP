d884cc04fa2e6076e442152ebfb35801878fa85d
Qaliss <pranaunaras12@gmail.com>
2025-08-19 21:32:40 -0500

    update navbar

--- mentalmath/src/components/NavBar.jsx
+++ mentalmath/src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-import "../css/NavBar.css"
+import "../css/Navbar.css"
 import { signOut } from "firebase/auth";
 import { auth } from "../firebase";
 import profilePic from '../assets/vector-flat-illustration-grayscale-avatar-600nw-2281862025.webp';
